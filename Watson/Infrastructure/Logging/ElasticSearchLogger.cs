5208e3b8a86a39711d2d395b6dab42849a295603
Pierre Gillon <gillon.pierre69@gmail.com>
2019-02-04 15:24:33 +0700

    Write in console line when error occurred during elastic search logging.

--- Watson/Infrastructure/Logging/ElasticSearchLogger.cs
+++ Watson/Infrastructure/Logging/ElasticSearchLogger.cs
@@ -21,10 +21,20 @@ namespace Watson.Infrastructure.Logging
         public async Task Log(MonitoringLogEntry logEntry)
         {
             var response = await _client.IndexAsync<StringResponse>("monitoring", logEntry.GetType().Name, logEntry.Id.ToString(), PostData.Serializable(logEntry));
+            CheckReponseSuccess(response, "monitoring");
         }
+
         public async Task Log(ErrorLogEntry logEntry)
         {
             var response = await _client.IndexAsync<StringResponse>("error", logEntry.GetType().Name, logEntry.Id.ToString(), PostData.Serializable(logEntry));
+            CheckReponseSuccess(response, "error");
+        }
+        
+        private static void CheckReponseSuccess(StringResponse response, string index)
+        {
+            if (response.Success == false) {
+                Console.WriteLine($"[{DateTimeOffset.UtcNow}] An error occured when logging on ElasticSearch, index '{index}'. {response.OriginalException}");
+            }
         }
     }
 }   
