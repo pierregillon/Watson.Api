3bac129a24cf4f8ee09a64c4cf96e8c80516544d
Pierre Gillon <gillon.pierre69@gmail.com>
2019-02-07 23:17:33 +0700

    Cancel requests while all events published.

--- Watson/Bootstrapper.cs
+++ Watson/Bootstrapper.cs
@@ -6,6 +6,11 @@ using CQRSlite.Events;
 using Watson.Infrastructure.Logging;
 using System;
 using Microsoft.Extensions.Configuration;
+using Nancy;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Watson.Server
 {
@@ -31,6 +36,45 @@ namespace Watson.Server
             var eventPublisher = container.GetInstance<IEventPublisher>();
             var settings = container.GetInstance<AppSettings>();
 
+            
+        }
+    }
+
+    public class LoadEventsStartup : IApplicationStartup
+    {
+        private readonly ConsoleLogger logger;
+        private readonly EventStoreOrg eventStore;
+        private readonly IEventPublisher eventPublisher;
+        private readonly AppSettings settings;
+
+        public LoadEventsStartup(
+            ConsoleLogger logger, 
+            EventStoreOrg eventStore, 
+            IEventPublisher eventPublisher,
+            AppSettings settings)
+        {
+            this.logger = logger;
+            this.eventStore = eventStore;
+            this.eventPublisher = eventPublisher;
+            this.settings = settings;
+        }
+
+        public async void Initialize(IPipelines pipelines)
+        {
+            pipelines.BeforeRequest.InsertItemAtPipelineIndex(0, new PipelineItem<Func<NancyContext, Response>>("LoadDomainEvents", InterceptRequests), false);
+
+            try
+            {
+                await ReadAndPublishAllEvents();
+            }
+            finally
+            {
+                pipelines.BeforeRequest.RemoveByName("LoadDomainEvents");
+            }
+        }
+
+        private async Task ReadAndPublishAllEvents()
+        {
             logger.WriteLine(ConsoleColor.DarkYellow, $"* Connecting to eventstore ...");
             await eventStore.Connect(
                 settings.EventStore.Server,
@@ -38,12 +82,28 @@ namespace Watson.Server
                 settings.EventStore.User,
                 settings.EventStore.Password
             );
-            logger.WriteLine(ConsoleColor.DarkYellow,$"* Reading all events from beginning ...");
+            
+            logger.WriteLine(ConsoleColor.DarkYellow, $"* Reading all events from beginning ...");
             var events = await eventStore.ReadAllEventsFromBeginning();
+            
+            logger.WriteLine(ConsoleColor.DarkYellow, $"* Publishing {events.Count()} events ...");
             foreach (var @event in events) {
                 await eventPublisher.Publish(@event);
             }
+            
             logger.WriteLine(ConsoleColor.DarkYellow,$"* DONE");
+        }
+
+        private Response InterceptRequests(NancyContext context)
+        {
+            return new Nancy.Response() {
+                StatusCode = HttpStatusCode.Processing,
+                ContentType = "text/plain",
+                Contents = x => {
+                    var bytes = UTF8Encoding.UTF8.GetBytes("Server loading");
+                    x.Write(bytes, 0, bytes.Length);
+                }
+            };
         }
     }
 }
